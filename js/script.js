a37eb95025dc76d599917bae3292ac2147846dc1
Cvele <cuekisa@yahoo.com>
2018-04-16 14:29:19 +0200

    a
    a

---
+++ js/script.js
@@ -0,0 +1,68 @@
+
+
+$(".proj1").click(function () { 
+	var leftPos = $('.works').scrollLeft();
+	console.log(leftPos+2);
+	// $(".works").animate({scrollLeft: leftPos + 100%}, 800);
+	$(".works").animate({ scrollLeft: 1*0.98}, 800);		
+  });
+
+  $(".proj2").click(function () { 
+	var leftPos = $('.works').scrollLeft();
+	console.log(leftPos+2);
+	// $(".works").animate({scrollLeft: leftPos + 100%}, 800);
+	$(".works").animate({ scrollLeft: $(".works").width()}, 800);		
+  });
+
+  $(".proj3").click(function () { 
+	var leftPos = $('.works').scrollLeft();
+	console.log(leftPos+2);
+	// $(".works").animate({scrollLeft: leftPos + 100%}, 800);
+	$(".works").animate({ scrollLeft: $(".works").width()*2}, 800);		
+  });
+
+  $(".proj4").click(function () { 
+	var leftPos = $('.works').scrollLeft();
+	console.log(leftPos+2);
+	// $(".works").animate({scrollLeft: leftPos + 100%}, 800);
+	$(".works").animate({ scrollLeft: $(".works").width()*3}, 800);		
+  });
+
+  $(".proj5").click(function () { 
+	var leftPos = $('.works').scrollLeft();
+	console.log(leftPos+2);
+	// $(".works").animate({scrollLeft: leftPos + 100%}, 800);
+	$(".works").animate({ scrollLeft: $(".works").width()*4}, 800);		
+  });
+
+  $(".proj6").click(function () { 
+	var leftPos = $('.works').scrollLeft();
+	console.log(leftPos+2);
+	// $(".works").animate({scrollLeft: leftPos + 100%}, 800);
+	$(".works").animate({ scrollLeft: $(".works").width()*5}, 800);		
+  });
+
+
+// Ellipse way - planets
+
+// window.requestAnimationFrame(anim1);
+
+// var a1 = document.getElementById('a1');
+
+// function anim1(t1) {
+
+//     a1.style.left = (50 + Math.cos(t1 / 11150) * 560) + 'px';
+//     a1.style.top = (50 + Math.sin(t1 /11150) * 40) + 'px';
+//     window.requestAnimationFrame(anim1);
+// }
+
+
+
+// 
+
+
+
+
+// ELLIPSE
+
+ 
